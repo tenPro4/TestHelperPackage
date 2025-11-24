3d72add5dcc44244c6b7eccd45bff4dcb48f52ec
Weng Shang Hoo <wengshang.hoo@my.rapidmfg.com>
2025-11-24 15:16:07 +0800

    first commit

---
+++ TestHelperPackage/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace TestHelperPackage
+{
+    public static class TestHelperPackage
+    {
+        public static string JoinSeperator(string input,string separator)
+        {
+            return string.Join(separator, input);
+        }
+    }
+}
